a2142db9ac242758186c2146b507f8b86cbf41e7
Erno Hentonen <erno.hentonen@opiframe.com>
2023-01-02 14:05:05 +0200

    custom hook

---
+++ react/04_simple_custom_hook/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import useCount from './hooks/useCount';
+function App() {
+	
+	const [value,add,substract] = useCount(10);
+	
+	return (
+		<div className="App">
+			<h2>Count:{value}</h2>
+			<button onClick={add}>+</button>
+			<button onClick={substract}>-</button>
+		</div>
+	);
+}
+
+export default App;
