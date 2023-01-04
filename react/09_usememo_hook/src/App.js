2d2e44a56a4e56a1d7bdecaa1f3f25ff8fe20eb3
Erno Hentonen <erno.hentonen@opiframe.com>
2023-01-04 11:07:57 +0200

    usememo

---
+++ react/09_usememo_hook/src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState,useMemo} from 'react';
+function App() {
+	
+	const [count,setCount] = useState(0);
+	
+	const [currentWord,setCurrentWord] = useState(0);
+ 	
+	const words = ["banaani","omena","olut","jäätelö"];
+	
+	const word = words[currentWord];
+	
+	const computeWordLength = (word) => {
+		let i=0;
+		while(i<1000000000) {
+			i++;
+		}
+		return word.length;
+	}
+	
+	//const wordLength = computeWordLength(word);
+	const wordLength = useMemo(() => computeWordLength(word),[word]);
+	return (
+		<div className="App">
+			<h3>Compute the length of the word {word}</h3>
+			<h3>{word} is {wordLength} letters long</h3>
+			<button onClick={() => {
+				const next = currentWord + 1 === words.length ? 0 : currentWord+1;
+				setCurrentWord(next);
+			}}>Next Word</button>
+			<br/>
+			<h3>Increment</h3>
+			<h3>Current value:{count}</h3>
+			<button onClick={() => setCount(count => count +1)}>+</button>
+		</div>
+	);
+}
+
+export default App;
