7f0078231f149bfabb01667b04921268d6803086
Erno Hentonen <erno.hentonen@opiframe.com>
2023-01-11 09:56:47 +0200

    redux test 2

---
+++ react/12_redux_test_2/src/components/ContactForm.js
@@ -0,0 +1,80 @@
+import {useState} from 'react';
+import {useDispatch} from 'react-redux';
+const ContactForm = (props) => {
+	
+	const [state,setState] = useState({
+		firstname:"",
+		lastname:"",
+		email:"",
+		phone:""
+	})
+	
+	const dispatch = useDispatch();
+	
+	const onChange = (event) => {
+		setState((state) => {
+			return {
+				...state,
+				[event.target.name]:event.target.value
+			}
+		})
+	}
+	
+	const onSubmit = (event) => {
+		event.preventDefault();
+		let contact = {
+			...state
+		}
+		dispatch({
+			type:"ADD_CONTACT",
+			contact:contact
+		})
+		setState({
+			firstname:"",
+			lastname:"",
+			email:"",
+			phone:""
+		})
+	}
+	return(
+		<div style={{
+			width:"40%",
+			backgroundColor:"lightgreen",
+			margin:"auto"
+		}}>
+			<form className="mb-3" onSubmit={onSubmit}>
+				<label htmlFor="firstname" className="form-label">First Name</label>
+				<input type="text"
+						name="firstname"
+						id="firstname"
+						className="form-control"
+						onChange={onChange}
+						value={state.firstname}/>
+				<label htmlFor="lastname" className="form-label">Last Name</label>
+				<input type="text"
+						name="lastname"
+						id="lastname"
+						className="form-control"
+						onChange={onChange}
+						value={state.lastname}/>
+				<label htmlFor="email" className="form-label">Email</label>
+				<input type="email"
+						name="email"
+						id="email"
+						className="form-control"
+						onChange={onChange}
+						value={state.email}/>
+				<label htmlFor="phone" className="form-label">Phone</label>
+				<input type="tel"
+						name="phone"
+						id="phone"
+						className="form-control"
+						onChange={onChange}
+						value={state.phone}/>
+				<input type="submit" className="btn btn-primary" value="Add"/>
+			</form>
+		</div>
+	)
+}
+
+export default ContactForm;
---
+++ react/12_redux_test_2/src/reducers/contactReducer.js
@@ -0,0 +1,26 @@
+const initialState = {
+	list:[],
+	id:100
+}
+
+const contactReducer = (state = initialState,action) => {
+	console.log("contactReducer, action",action);
+	switch(action.type) {
+		case "ADD_CONTACT":
+			action.contact.id = state.id
+			return {
+				list:state.list.concat(action.contact),
+				id:state.id+1
+			}
+		case "REMOVE_CONTACT":
+			let tempList = state.list.filter(contact => contact.id !== action.id);
+			return {
+				...state,
+				list:tempList
+			}
+		default:
+			return state;
+	}
+}
+
+export default contactReducer;
