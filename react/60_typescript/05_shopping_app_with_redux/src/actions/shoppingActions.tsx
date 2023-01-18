51cf613f208882e2d90c57e9b45d20d3627eaf16
Erno Hentonen <erno.hentonen@opiframe.com>
2023-01-18 12:31:04 +0200

    typescript redux

--- react/60_typescript/05_shopping_app_with_redux/src/actions/shoppingActions.tsx
+++ react/60_typescript/05_shopping_app_with_redux/src/actions/shoppingActions.tsx
@@ -6,6 +6,57 @@ import * as actionConstants from '../types/actionConstants';
 
 //ASYNC THUNKS
 
+export const getList = (token:string) => {
+	return (dispatch:ThunkDispatch<any,any,AnyAction>) => {
+		let request = new Request("/api/shopping",{
+			method:"GET",
+			headers:{
+				"token":token
+			}
+		})
+		handleFetch(request,"getlist",dispatch,token)
+	}
+}
+
+export const add = (token:string,item:ShoppingItem) => {
+	return (dispatch:ThunkDispatch<any,any,AnyAction>) => {
+		let request = new Request("/api/shopping",{
+			method:"POST",
+			headers:{
+				"Content-type":"application/json",
+				"token":token
+			},
+			body:JSON.stringify(item)
+		})
+		handleFetch(request,"add",dispatch,token)
+	}
+}
+
+export const remove = (token:string,id:number) => {
+	return (dispatch:ThunkDispatch<any,any,AnyAction>) => {
+		let request = new Request("/api/shopping/"+id,{
+			method:"DELETE",
+			headers:{
+				"token":token
+			}
+		})
+		handleFetch(request,"remove",dispatch,token)
+	}
+}
+
+export const edit = (token:string,item:ShoppingItem) => {
+	return (dispatch:ThunkDispatch<any,any,AnyAction>) => {
+		let request = new Request("/api/shopping/"+item.id,{
+			method:"PUT",
+			headers:{
+				"Content-type":"application/json",
+				"token":token
+			},
+			body:JSON.stringify(item)
+		})
+		handleFetch(request,"edit",dispatch,token)
+	}
+}
 
 const handleFetch = async (request:Request,act:string,dispatch:ThunkDispatch<any,any,AnyAction>,token:string) => {
 	dispatch(loading());
@@ -42,7 +93,27 @@ const handleFetch = async (request:Request,act:string,dispatch:ThunkDispatch<any
 				return;
 		}
 	} else {
-		
+		if(response.status === 403) {
+			dispatch(logoutFailed("Your session has expired. Logging you out."))
+			return;
+		}
+		let errorMessage = " Server responded with a status "+response.status+" "+response.statusText
+		switch(act) {
+			case "getlist":
+				dispatch(fetchItemFailed(actionConstants.FETCH_LIST_FAILED,"Failed to fetch shopping list."+errorMessage))
+				return;
+			case "add":
+				dispatch(fetchItemFailed(actionConstants.ADD_ITEM_FAILED,"Failed to add new item."+errorMessage))
+				return;
+			case "remove":
+				dispatch(fetchItemFailed(actionConstants.REMOVE_ITEM_FAILED,"Failed to remove item."+errorMessage))
+				return;
+			case "edit":
+				dispatch(fetchItemFailed(actionConstants.EDIT_ITEM_FAILED,"Failed to edit item."+errorMessage))
+				return;
+			default:
+				return;
+		}
 	}
 }
 
