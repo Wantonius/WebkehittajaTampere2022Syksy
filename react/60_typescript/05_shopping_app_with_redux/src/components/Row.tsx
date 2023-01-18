79af48facbabdbf5347e20c6dba22298e9a16bcb
Erno Hentonen <erno.hentonen@opiframe.com>
2023-01-18 14:06:48 +0200

    typescript redux

---
+++ react/60_typescript/05_shopping_app_with_redux/src/components/Row.tsx
@@ -0,0 +1,23 @@
+import ShoppingItem from '../models/ShoppingItem';
+import React from 'react';
+
+interface Props {
+	item:ShoppingItem;
+	index:number;
+	changeMode:(mode:string,index:number) => void;
+}
+
+const Row:React.FC<Props> = (props:Props) => {
+	
+	return(
+		<tr>
+			<td>{props.item.type}</td>
+			<td>{props.item.count}</td>
+			<td>{props.item.price}</td>
+			<td><button onClick={() => props.changeMode("remove",props.index)}>Remove</button></td>
+			<td><button onClick={() => props.changeMode("edit",props.index)}>Edit</button></td>
+		</tr>
+	)
+}
+
+export default Row;
