5e16686569d74c45fc3ce57c1a69c7fa77fe1df2
Erno Hentonen <erno.hentonen@opiframe.com>
2023-01-19 09:41:13 +0200

    typescript redux

--- react/60_typescript/05_shopping_app_with_redux/src/App.tsx
+++ react/60_typescript/05_shopping_app_with_redux/src/App.tsx
@@ -3,14 +3,44 @@ import logo from './logo.svg';
 import './App.css';
 import LoginPage from './components/LoginPage';
 import ShoppingForm from './components/ShoppingForm';
+import ShoppingList from './components/ShoppingList';
+import Navbar from './components/Navbar';
+import {Routes,Route,Navigate} from 'react-router-dom';
+import {useSelector} from 'react-redux';
+import {AppState} from './types/states';
 
 function App() {
-  return (
-    <div className="App">
-		<LoginPage/>
-		<ShoppingForm/>
-    </div>
-  );
+	
+	const stateSelector = (state:AppState) => state;
+	
+	const appState = useSelector(stateSelector);
+	
+	if(appState.login.isLogged) {
+		return (
+			<div className="App">
+				<Navbar/>
+				<hr/>
+				<Routes>
+					<Route path="/" element={<ShoppingList/>}/>
+					<Route path="/form" element={<ShoppingForm/>}/>
+					<Route path="*" element={<Navigate to="/"/>}/>
+				</Routes>
+			</div>
+		);		
+	} else {
+		return (
+			<div className="App">
+				<Navbar/>
+				<hr/>
+				<Routes>
+					<Route path="/" element={<LoginPage/>}/>
+					<Route path="*" element={<Navigate to="/"/>}/>
+				</Routes>
+			</div>
+		);	
+	}
+	
+
 }
 
 export default App;
