d17b95b5ef8eb812ed34792c5b9d91ff2218c0ff
Erno Hentonen <erno.hentonen@opiframe.com>
2023-03-21 13:52:25 +0200

    shopping app with backend

---
+++ react-native/07_shopping_app_with_backend/App.js
@@ -0,0 +1,201 @@
+import {useState,useEffect} from 'react';
+import ShoppingList from './components/ShoppingList';
+import ShoppingForm from './components/ShoppingForm';
+import LoginPage from './components/LoginPage';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+
+	const [state,setState] = useState({
+		list:[],
+		isLogged:false,
+		token:""
+	})
+	
+	const [urlRequest,setUrlRequest] = useState({
+		url:"",
+		request:{},
+		action:""
+	})
+	
+	useEffect(() => {
+		
+		const fetchData = async () => {
+			if(urlRequest.url === "") {
+				return;
+			}
+			let url = "https://average-sarong-slug.cyclic.app"+urlRequest.url;
+			const response = await fetch(url,urlRequest.request);
+			if(!response) {
+				return;
+			}
+			if(response.ok) {
+				switch(urlRequest.action) {
+					case "register":
+						alert("Register success");
+						return;
+					case "login":
+						const data = await response.json();
+						if(!data) {
+							return;
+						}
+						setState((state) => {
+							return {
+								...state,
+								isLogged:true,
+								token:data.token
+							}
+						})
+						getList(data.token);
+						return;
+					case "logout":
+						setState({
+							list:[],
+							isLogged:false,
+							token:""
+						})
+						return;
+					case "getlist":
+						const list = await response.json();
+						if(!list) {
+							return;
+						}
+						setState((state) => {
+							return {
+								...state,
+								list:list
+							}
+						})
+						return;
+					case "additem":
+					case "removeitem":
+						getList(state.token);
+						return;
+					default:
+						return;
+				}
+			} else {
+				if(response.status === 403) {
+					setState({
+						list:[],
+						isLogged:false,
+						token:""
+					})
+				}
+				if(urlRequest.action === "logout") {
+					setState({
+						list:[],
+						isLogged:false,
+						token:""
+					})
+				}
+			}
+		}
+		
+		fetchData();
+	},[urlRequest])
+
+	const register = (user) => {
+		setUrlRequest({
+			url:"/register",
+			request:{
+				method:"POST",
+				headers:{"Content-Type":"application/json"},
+				body:JSON.stringify(user)
+			},
+			action:"register"
+		})
+	}
+	
+	const login = (user) => {
+		setUrlRequest({
+			url:"/login",
+			request:{
+				method:"POST",
+				headers:{"Content-Type":"application/json"},
+				body:JSON.stringify(user)
+			},
+			action:"login"
+		})
+	}
+	
+	const logout = () => {
+		setUrlRequest({
+			url:"/logout",
+			request:{
+				method:"POST",
+				headers:{"token":state.token}
+			},
+			action:"logout"
+		})
+	}
+	
+	const getList = (token) => {
+		setUrlRequest({
+			url:"/api/shopping",
+			request:{
+				method:"GET",
+				headers:{
+					"token":token
+				}
+			},
+			action:"getlist"
+		})
+	}
+	
+	const addToList = (item) => {
+		setUrlRequest({
+			url:"/api/shopping",
+			request:{
+				method:"POST",
+				headers:{
+					"Content-Type":"application/json",
+					"token":state.token
+				},
+				body:JSON.stringify(item)
+			},
+			action:"additem"
+		})
+	}
+	
+	const removeItem = (id) => {
+		setUrlRequest({
+			url:"/api/shopping/"+id,
+			request:{
+				method:"DELETE",
+				headers:{
+					"token":state.token
+				}
+			},
+			action:"removeitem"
+		})
+	}
+	
+	return(
+		<NavigationContainer>
+			<Stack.Navigator>
+			{state.isLogged ? (
+				<>
+					<Stack.Screen name="ShoppingList">
+					{props => <ShoppingList {...props} list={state.list} removeItem={removeItem} logout={logout}/>}
+					</Stack.Screen>
+					<Stack.Screen name="Add Item">
+					{props => <ShoppingForm {...props} addToList={addToList}/>}
+					</Stack.Screen>
+				</>
+			):(
+				<>
+					<Stack.Screen name="Login">
+					{props => <LoginPage {...props} login={login} register={register}/>}
+					</Stack.Screen>
+				</>
+			)
+			}			
+			</Stack.Navigator>
+		</NavigationContainer>
+	)
+}
+
---
+++ react-native/07_shopping_app_with_backend/components/ShoppingList.js
@@ -0,0 +1,85 @@
+import {FlatList,View,Pressable,Text,StyleSheet} from 'react-native'
+
+const ShoppingList = (props) => {
+	return(
+		<View style={styles.container}>
+			<View style={styles.buttonBox}>
+				<Pressable style={styles.navigateButton}
+					onPress={() => props.navigation.navigate("Add Item")}>
+					<Text style={styles.text}>Add new item</Text>
+				</Pressable>
+				<Pressable style={[styles.navigateButton,styles.logoutButton]}
+					onPress={() => props.logout()}>
+					<Text style={styles.text}>Logout</Text>
+				</Pressable>				
+			</View>
+			<View style={styles.listBox}>
+				<FlatList data={props.list}
+						renderItem={
+							({item}) => {
+								return(
+									<View style={styles.row}>
+										<Text style={styles.text}>Type:{item.type}</Text>
+										<Text style={styles.text}>Count:{item.count}</Text>
+										<Text style={styles.text}>Price:{item.price}</Text>
+										<Pressable style={styles.removeButton} onPress={() => props.removeItem(item.id)}>
+											<Text style={styles.text}>Remove</Text>
+										</Pressable>
+									</View>
+								)
+							}
+						}
+						keyExtractor={(item) => ""+item.id}
+						/>
+			</View>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	container:{
+		flex:1
+	},
+	buttonBox:{
+		flex:1,
+		justifyContent:"space-evenly",
+		flexDirection:"row",
+		alignItems:"center"
+	},
+	listBox:{
+		flex:10,
+		justifyContent:"center",
+		alignItems:"center"
+	},
+	navigateButton:{
+		flex:1,
+		justifyContent:"center",
+		alignItems:"center",
+		backgroundColor:"blue"
+	},
+	logoutButton:{
+		backgroundColor:"green"
+	},
+	text:{
+		fontFamily:"sans-serif",
+		fontWeight:"bold",
+		fontSize:13,
+		padding:2
+	},
+	removeButton:{
+		padding:3,
+		width:70,
+		height:50,
+		backgroundColor:"red",
+		alignItems:"center",
+		justifyContent:"center"
+	},
+	row:{
+		flex:1,
+		flexDirection:"row",
+		justifyContent:"space-evenly",
+		alignItems:"center"
+	}
+})
+
+export default ShoppingList;
